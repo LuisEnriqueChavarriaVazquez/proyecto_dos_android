dec93c3ce15b7e4c2048896c6aa59322c48a89aa
Luis Enrique <luisechvz@gmail.com>
2021-05-24 15:38:42 -0500

    nuevo 2

--- conn.php
+++ conn.php
@@ -10,4 +10,18 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 echo "Connected successfully";
-?>
+
+$sql = "SELECT id, volumen FROM volumenTable";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+// output data of each row
+while($row = mysqli_fetch_assoc($result)) {
+    echo "id: " . $row["id"]. " Volumen: " . $row["volumen"] . "<br>";
+    echo "Gato";
+}
+} else {
+echo "0 results";
+}
+
+mysqli_close($conn);
--- index.php
+++ index.php
@@ -12,20 +12,7 @@
 
         include 'conn.php';
 
-        $sql = "SELECT id, volumen FROM volumenTable";
-        $result = mysqli_query($conn, $sql);
-
-        if (mysqli_num_rows($result) > 0) {
-        // output data of each row
-        while($row = mysqli_fetch_assoc($result)) {
-            echo "id: " . $row["id"]. " Volumen: " . $row["volumen"] . "<br>";
-            echo "Gato";
-        }
-        } else {
-        echo "0 results";
-        }
-
-        mysqli_close($conn);
+        
             ?>
 
     
