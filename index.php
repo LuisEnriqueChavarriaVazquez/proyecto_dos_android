60f656f0d041bb9088a1ce07933bcd6d90997bca
Luis Enrique <luisechvz@gmail.com>
2021-05-24 15:34:30 -0500

    nuevo 2

--- index.php
+++ index.php
@@ -11,7 +11,21 @@
         echo "<h2>Hola!!</h2>";
 
         include 'conn.php';
-    ?>
+
+        $sql = "SELECT id, volumen FROM volumenTable";
+        $result = mysqli_query($conn, $sql);
+
+        if (mysqli_num_rows($result) > 0) {
+        // output data of each row
+        while($row = mysqli_fetch_assoc($result)) {
+            echo "id: " . $row["id"]. " Volumen: " . $row["volumen"] . "<br>";
+        }
+        } else {
+        echo "0 results";
+        }
+
+        mysqli_close($conn);
+            ?>
 
     
 </body>
