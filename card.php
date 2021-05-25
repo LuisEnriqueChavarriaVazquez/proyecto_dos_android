d4ded42e780a59dc14d636d9eaab6565f6f64399
Luis Enrique <luisechvz@gmail.com>
2021-05-25 01:55:10 -0500

    diseño de los nombres de los integrantes del team

--- card.php
+++ card.php
@@ -10,5 +10,26 @@
              style="width: 100%;">
               <i class="material-icons left">refresh</i>Refrescar
           </a>
+
+          <!-- Modal Trigger -->
+            <a class="waves-effect waves-light btn white blue-text modal-trigger" href="#modal1">
+                <i class="material-icons left">refresh</i>Integrantes
+            </a>
       </div>
+  </div>
+
+  
+
+  <!-- Modal Structure -->
+  <div id="modal1" class="modal">
+    <div class="modal-content">
+      <h4>Datos del equipo.</h4>
+      <p>* Chavarría Vázquez Luis Enrique. <br>
+      * Machorro Vences Ricardo Alberto
+      * Juarez Espinosa Ulises.
+      * Esquivel Luna Adan.</p>
+    </div>
+    <div class="modal-footer">
+      <a href="#!" class="modal-close waves-effect waves-green btn-flat">Listo</a>
+    </div>
   </div>
--- index.php
+++ index.php
@@ -19,5 +19,7 @@
     
 </body>
     <!-- Compiled and minified JavaScript -->
+    <script src="jquery.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script src="codigo.js"></script>
 </html>
