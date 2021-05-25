957da37e7cb076e38c4aa6fa4668b4ac24de793f
Luis Enrique <luisechvz@gmail.com>
2021-05-25 01:39:55 -0500

    diseño de la pagina

--- card.php
+++ card.php
@@ -1,9 +1,13 @@
-<div class="row">
+  <div class="row">
     <div class="col s12 m12">
-      <div class="card white">
-        <div class="card-content black-text">
-          <?php include 'conn.php';?>
-        </div>
+      <div class="card-panel teal">
+        <span class="white-text">
+            <?php include 'conn.php';?>
+        </span>
       </div>
     </div>
   </div>
+
+  <a href="https://proyectodosescomsupersonico.herokuapp.com/" class="s12 m12 waves-effect waves-light btn white blue-text">
+    <i class="material-icons left">cloud</i>Refrescar
+  </a>
--- conn.php
+++ conn.php
@@ -16,7 +16,7 @@ $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
 // output data of each row
 while($row = mysqli_fetch_assoc($result)) {
-    echo "<br><h3>" . "Vol: " . $row["volumen"] . "</h3><br>";
+    echo "<br><h4>" . "Vol: " . $row["volumen"] . "</h4><br>";
 }
 } else {
 echo "0 results";
