585ab1adf2a4e49125c6b422886b16fb9e7c8e73
Luis Enrique <luisechvz@gmail.com>
2021-05-25 01:46:59 -0500

    diseño de la pagina

--- menu.php
+++ menu.php
@@ -1,6 +1,6 @@
 <nav>
     <div class="nav-wrapper light-blue darken-4">
-      <a href="#" class="brand-logo center">Proyecto dos</a>
+      <a href="#" class="brand-logo center"><i class="material-icons left">settings_remote</i></a>
       <ul id="nav-mobile" class="left hide-on-med-and-down">
         <li><a href="sass.html"></a></li>
         <li><a href="badges.html"></a></li>
