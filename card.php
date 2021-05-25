9b8b979b1d31b92aca408399e4a92b5a753bc934
Luis Enrique <luisechvz@gmail.com>
2021-05-25 01:42:41 -0500

    diseño de la pagina

--- card.php
+++ card.php
@@ -5,7 +5,9 @@
                   <?php include 'conn.php'; ?>
               </span>
           </div>
-          <a href="https://proyectodosescomsupersonico.herokuapp.com/" class="s12 m12 waves-effect waves-light btn white blue-text">
+          <a href="https://proyectodosescomsupersonico.herokuapp.com/" 
+             class="s12 m12 waves-effect waves-light btn white blue-text"
+             style="width: 100%;">
               <i class="material-icons left">cloud</i>Refrescar
           </a>
       </div>
